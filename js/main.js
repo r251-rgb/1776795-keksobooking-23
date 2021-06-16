9ad3464ee50fe866546e6a002b7c367612d5d7bb
Eugeniy <zl-dog@mail.ru>
2021-06-16 14:26:40 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 import {createCardArray} from '../js/create-card.js'; //функция генерации карточек
+import {generateCardElement} from '../js/make-card.js'; //функция генерации карточек
 
-//console.log(createCardArray);
-createCardArray.find();
+
+//generateCardElement(createCardArray);
+
+//первый элемент массива для отрисовки
+const tmp = createCardArray[0];
+
+document.querySelector('#map-canvas').appendChild(generateCardElement(tmp));
